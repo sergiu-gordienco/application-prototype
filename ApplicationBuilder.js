ddcde88429e7b243ff33f697286bd5540b6271d6
Sergiu Gordienco Vasile <sergiu.gordienco@gmail.com>
2017-01-06 06:48:39 +0200

    Fix Error for NodeJS integration

--- ApplicationBuilder.js
+++ ApplicationBuilder.js
@@ -2,6 +2,7 @@
 // jshint -W002
 // jshint -W083
 // jshint -W069
+// jshint -W061
 var isNode	= ( typeof global !== "undefined" &&
 	typeof process !== "undefined" &&
 	{}.toString.call(global) == '[object global]' );
@@ -24,6 +25,8 @@ var ApplicationBuilder	= function (callback) {
 			callback	= false;
 		}
 	}
+
+	if (isNode) ApplicationPrototype = require('./ApplicationPrototype.js');
 	var Application	= new ApplicationPrototype(function (
 			configurations,
 			variables,
@@ -374,7 +377,9 @@ var ApplicationBuilder	= function (callback) {
 				var module_count	= 0;
 				var module_length	= module_name.length;
 				var module_errors	= [];
+				// jshint -W004
 				var $request	= Application.Promise();
+				// jshint +W004
 				module_name.forEach(function (module_name) {
 					var module_link	= false;
 					((function (m) {
@@ -404,6 +409,7 @@ var ApplicationBuilder	= function (callback) {
 			}
 		});
 	})());
+	console.error("Application", Application);
 	if (typeof(params.callback_ready) === "function") {
 		var er;
 		try {
@@ -427,6 +433,8 @@ var ApplicationBuilder	= function (callback) {
 // │ Attribution-NonCommercial-NoDerivs 3.0 Unported License.                                      │ \\
 // │ To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-nd/3.0/.      │ \\
 // └───────────────────────────────────────────────────────────────────────────────────────────────┘ \\
+// jshint -W033
+// jshint -W032
 ;((function (window) {
 eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a)>35?String.fromCharCode(c+29):c.toString(36));};if(!''.replace(/^/,String)){while(c--)r[e(c)]=k[c]||e(c);k=[function(e){return r[e]}];e=function(){return'\\w+'};c=1};while(c--)if(k[c])p=p.replace(new RegExp('\\b'+e(c)+'\\b','g'),k[c]);return p}('6.u=B(o,k){p r=[],i;5(!k)k="";5(1g.1h(o)&&k){v(i=0;i<o.C;i++)r.N(u(o[i],""+k+"["+i+"]"))}w 5(y(o)=="O"){v(i q o)r.N(u(o[i],""+k+(k?"[":"")+i+(k?"]":"")))}w 5(k){z""+k+"="+R(o)}z r.S(\'&\')};6.T=B(c,d,f,g,h,j,k,l,m){5(2===6)z G T(c,d,f,g,h,j,k,l,m);2.8=\'1i\'+G 1j().1k()+\'1l\'+Z.1m(Z.1n()*1o);6[2.8]=2;5(!f)f={};5(!k)k=(!h?\'H\':\'I\');5(!l)l=\'9\';p n={"U":"U","10":"10","11":"11","12":"12","9":"9","V":"U"};2.A=(l q n)?l:"9";2.W=f;2.X=k;2.J=c;2.13=d;2.D=(y(f)=="O"&&"D"q f&&y(f["D"])=="B"?f["D"]:Y);2.9=1p;2.14=!!m;2.E=(y(g)=="O"?g:{});2.F=(y(h)=="O"?h:{});2.P=B(){p e;K{1q(2.1r)}L(e){};K{p b=2.J,9=2.9,f=2.W,H=2.E,I=2.F,s=2.s,k=2.X;5(2.A=="V"){p i,a=G 15(2.7.Q),t="";v(i=0;i<a.C;i++)t+=16.17(a[i]%18);9=t};(2.13)(b,9,f,H,I,s,j,k,2.A,2.7)}L(e){};K{1s 6[2.8]}L(e){}};2.19=B(){5(!2.D)z Y;K{p b=2.J,9=2.9,f=2.W,H=2.E,I=2.F,s=2.s,k=2.X;5(2.A=="V"){p i,a=G 15(2.7.Q),t="";v(i=0;i<a.C;i++)t+=16.17(a[i]%18);9=t};(2.D)(b,9,f,H,I,s,j,k,2.A,2.7)}L(e){1t.1u(\'T » P\',e)}};2.7=G 1v();p r=[];p i;5(\'u\'q 6){r=u(2.E)}w{v(i q 2.E)r.N(i+\'=\'+R(\'\'+2.E[i]));r=r.S(\'&\')};2.s=2.J+(r.C?(2.J.1w(\'?\')==-1?\'?\':(r.C?\'&\':\'\')):\'\')+r;5(2.14){2.7.1a=M;2.7.1b(k,2.s,M)}w{5(y(m)=="1c"){2.7.1a=M}2.7.1b(k,2.s)}2.7.A=n[2.A];5(\'u\'q 6){r=u(2.F)}w{r=[];v(i q 2.F)r.N(i+\'=\'+R(\'\'+2.F[i]));r=r.S(\'&\')};5(y(j)==="1c"){K{2.7.1d("1x-1y","1z/x-1A-1B-1C; 1D=1E-1F-1")}L(i){}}5(j){v(i=0;i<j.C;i++)2.7.1d(j[i].1G,j[i].1H)};1I("2.7.1J = B() {p 8 = \\""+2.8+"\\";5(!(8 q 6)) z Y;\\n		5(6[8].7.1e == 4){6[8].9	= 6[8].7.Q;6[8].P();}\\n		w 5(6[8].7.1e == 3){6[8].9	= 6[8].7.Q;6[8].19();}\\n		w 5( (\'1f\' q 6[8] ) && ( 6[8].1f == 1K ) ) {6[8].P();}	z M; }");2.7.1L(r);z M}',62,110,'||this|||if|window|link|id|text||||||||||||||||var|in||fullUrl||objEncodeURL|for|else||typeof|return|responseType|function|length|onloading_f|get_data|post_data|new|get|post|url|try|catch|true|push|object|endEval|response|encodeURIComponent|join|m_urlload|arraybuffer|binary|vars|method_r|false|Math|blob|document|json|onload_f|sessionConnected|Uint8Array|String|fromCharCode|256|tmpEval|withCredentials|open|undefined|setRequestHeader|readyState|status|Array|isArray|load_urldata_|Date|valueOf|_|floor|random|1000000|null|clearInterval|timer|delete|console|error|XMLHttpRequest|indexOf|Content|Type|application|www|form|urlencoded|charset|ISO|8859|name|value|eval|onreadystatechange|404|send'.split('|'),0,{}));
 })((function () {
@@ -438,8 +446,9 @@ eval(function(p,a,c,k,e,r){e=function(c){return(c<a?'':e(parseInt(c/a)))+((c=c%a
 		return APP_BUILDER_GLOBAL;
 	}
 })()));
-
+// jshint +W033
+// jshint +W032
 
 if (isNode) {
-	module.exports	= ApplicationPrototype;
+	module.exports	= ApplicationBuilder;
 }
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 module.exports = {
 	application	: require("./ApplicationPrototype.js"),
 	builder		: require("./ApplicationBuilder.js")
-}
+};
