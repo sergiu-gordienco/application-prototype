004fa70351f0941218b872855b010d33ae985a5a
Alina Gordienco <alina.gordienco@sgapps.io>
2023-02-27 15:57:14 +0200

    Author:    Alina Gordienco <alina.gordienco@sgapps.io>
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   constructors/graphic/filters/blur.js

--- constructors/graphic/filters/blur.js
+++ constructors/graphic/filters/blur.js
@@ -1,16 +1,21 @@
-module.exports = function ( imageData, offset, alpha ) {
+module.exports = function ( imageData, offset, alpha, repeats ) {
 	// TODO refactor
 	if (offset === 0) return imageData;
+	if (!repeats) repeats = 1;
 	var canvas = document.createElement('canvas');
+	var ctx = canvas.getContext('2d');
+	var i, repeat;
 	canvas.width	= imageData.width;
 	canvas.height	= imageData.height;
-	var ctx = canvas.getContext('2d');
 	ctx.globalAlpha = alpha || 0.3;
 	ctx.putImageData(imageData, 0, 0);
 	offset = offset || 3;
-	for (var i=1; i<=8;i++) {
-		ctx.drawImage(canvas, offset, 0, canvas.width-offset, canvas.height, 0, 0,canvas.width-offset, canvas.height);
-		ctx.drawImage(canvas, 0, offset, canvas.width, canvas.height-offset, 0, 0,canvas.width, canvas.height-offset);
+	
+	for (repeat=0; repeat<repeats;repeat++) {
+		for (i=1; i<=8;i++) {
+			ctx.drawImage(canvas, offset, 0, canvas.width-offset, canvas.height, 0, 0,canvas.width-offset, canvas.height);
+			ctx.drawImage(canvas, 0, offset, canvas.width, canvas.height-offset, 0, 0,canvas.width, canvas.height-offset);
+		}
 	}
 	ctx.globalAlpha = 1;
 	return ctx.getImageData(0, 0, imageData.width, imageData.height);
